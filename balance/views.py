ffea0915a340e2aef1eefd0a3769a21c7d55f04e
Tony G. Bolaño <tony.gonzalez@tyris-software.com>
2024-11-21 21:30:01 +0100

    feat: agregamos los errores en el formulario

--- balance/views.py
+++ balance/views.py
@@ -42,24 +42,28 @@ def actualizar(id):
         lista = ListaMovimientosDB()
         formulario = MovimientoForm(data=request.form)
 
-        fecha = formulario.fecha.data
-        mov_dict = {
-            'fecha': fecha.isoformat(),
-            'concepto': formulario.concepto.data,
-            'tipo': formulario.tipo.data,
-            'cantidad': formulario.cantidad.data,
-            'id': formulario.id.data
-        }
+        if formulario.validate():
+            fecha = formulario.fecha.data
+            mov_dict = {
+                'fecha': fecha.isoformat(),
+                'concepto': formulario.concepto.data,
+                'tipo': formulario.tipo.data,
+                'cantidad': formulario.cantidad.data,
+                'id': formulario.id.data
+            }
 
-        movimiento = Movimiento(mov_dict)
+            movimiento = Movimiento(mov_dict)
 
-        resultado = lista.editarMovimiento(movimiento)
+            resultado = lista.editarMovimiento(movimiento)
 
-        if resultado == 1:
-            template = 'guardado_ok.html'
-        elif resultado == -1:
-            template = 'error_edit.html'
+            if resultado == 1:
+                template = 'guardado_ok.html'
+            elif resultado == -1:
+                template = 'error_edit.html'
+            else:
+                template = 'error_inesperado.html'
         else:
-            template = 'error_inesperado.html'
+            print(formulario.errors)
+            return render_template('form_movimiento.html', form=formulario, id=formulario.id.data)
 
     return render_template(template)
