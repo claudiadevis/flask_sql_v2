675066fd42fd1fddf15902058d4d199d92955fe4
Tony G. Bolaño <tony.gonzalez@tyris-software.com>
2024-11-21 21:11:16 +0100

    feat: agregamos validaciones al formulario y las usamos en el navegador

--- balance/forms.py
+++ balance/forms.py
@@ -7,13 +7,24 @@ from wtforms import (
     StringField,
     SubmitField
 )
+from wtforms.validators import DataRequired, NumberRange
 
 
 class MovimientoForm(FlaskForm):
     id = HiddenField()
-    fecha = DateField('Fecha')
-    concepto = StringField('Concepto')
-    tipo = RadioField(choices=[('I', 'Ingreso'), ('G', 'Gasto')])
-    cantidad = DecimalField('Cantidad', places=2)
+    fecha = DateField('Fecha', validators=[
+        DataRequired('Debes indicar la fecha del movimiento')
+    ])
+    concepto = StringField('Concepto', validators=[
+        DataRequired('No has especificado un concepto para este movimiento')
+    ])
+    tipo = RadioField(choices=[('I', 'Ingreso'), ('G', 'Gasto')], validators=[
+        DataRequired('Necesito saber si es un gasto o un ingreso')
+    ])
+    cantidad = DecimalField('Cantidad', places=2, validators=[
+        DataRequired('No puede haber un movimiento sin una cantidad asociada'),
+        NumberRange(
+            min=0.1, message='No se permiten cantidades inferiores a 10 centimos')
+    ])
 
     submit = SubmitField('Guardar')
